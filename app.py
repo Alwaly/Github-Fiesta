ddbe052f38669665abdfad395236b37e7f2d807d
Alwaly <ndiayealwaly999@gmail.com>
2023-08-15 14:13:58 +0000

    Preprocessing and model

--- app.py
+++ app.py
@@ -1,44 +1,48 @@
 import pandas as pd
-import numpy as np
+import pickle as pkl
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
-import seaborn as sb
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv("douanesDataset.csv")
 
 df = data.drop_duplicates()
-min_count=df['ILLICIT'].value_counts().min()
-
-balanced_df = df.groupby('ILLICIT').apply(lambda x: x.sample(min_count)).reset_index(drop=True)
-
 df.dropna(inplace=True)
 
-df.drop(['YEAR','HS4', 'HS2', 'HS6.Origin', 'TARIFF.CODE', 'DECLARANT.CODE','OFFICEIMPORTER.TIN', 'SGD.MonthofYear'], axis=1, inplace=True)
+df.drop(['YEAR'], axis=1, inplace=True)
 
 min_count=df['ILLICIT'].value_counts().min()
 
-balanced_df = df.groupby('ILLICIT').apply(lambda x: x.sample(min_count)).reset_index(drop=True)
+df = df.groupby('ILLICIT').apply(lambda x: x.sample(min_count)).reset_index(drop=True)
 
-labels=balanced_df["ILLICIT"]
+Y = df['ILLICIT']
+X = df.drop('ILLICIT', axis=1)
 
-features=balanced_df.drop(['ILLICIT'], axis=1)
+continuous_features = X.select_dtypes(include=['float64', 'int64']).columns
+categorical_features = X.select_dtypes(include=['object']).columns
 
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.1, random_state=42)
+df_continuous = X[continuous_features]
 
-scaler = StandardScaler()
+df_categorical = X[categorical_features]
 
-scaler.fit(x_train)
+X_ = X.drop(categorical_features, axis=1)
+X = X.drop(continuous_features, axis=1)
 
-x_train_norm = scaler.transform(x_train)
-x_test_norm = scaler.transform(x_test)
+label_encoder = LabelEncoder()
+
+for column in categorical_features:
+    X[column] = label_encoder.fit_transform(X[column])
+  
+X_ = StandardScaler().fit_transform(X_)
+
+X_ = pd.DataFrame(X_, columns=continuous_features)
+X = pd.concat([X_,X], axis=1, ignore_index=True)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
 
 def gridsearch_dit(models, params,train_x, train_y):
   model_best=[]
@@ -76,8 +80,10 @@ parametres.append(param_grid_rf)
 parametres.append(param_grid_knn)
 parametres.append(param_grid_log)
 
-best_model= gridsearch_dit(classifiers, parametres, X_train, Y_train)
+best_model= gridsearch_dit(classifiers, parametres, x_train, y_train)
+
 mod, accu = best_model
 for i in range(len(mod)):
   if accu[i]== max(accu):
-    mod[i].save('model.h5')
+    with open("model.pkl", 'wb') as model_file:
+      pkl.dump(mod[i], model_file)
